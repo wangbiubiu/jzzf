339db90c07b01e6ec90b10e0b63c49b05dade538
aa <907810344@qq.cp>
2017-10-31 17:31:31 +0800

    1

--- Cashier/pigcms/Lib/Merchants/User/settlement.class.php
+++ Cashier/pigcms/Lib/Merchants/User/settlement.class.php
@@ -20,10 +20,7 @@ class settlement_controller extends common_controller
             $mid = $this->mid;//商家编号
             $qrcode=M('cashier_qrcode')->get_one(array('mid'=>$mid),'qrcode_id');
             $qrcode=$qrcode['qrcode_id'];
-            $amount = $huazhangmoney;//申请代付金额
-            $dmount = $record['money'];//代付金额
-            $rmount=$amount<=$dmount?$amount:$dmount;
-            $url = 'http://b.jizhipay.com/merchants.php?m=pay&c=jhzdf&a=jdf';  //调用接口的平台服务地址
+            $url = 'http://b.jizhipay.com/merchants.php?m=Pay&c=jhzdf&a=jdf';  //调用接口的平台服务地址
             $post_string = array(
                 'ewmid'=>$qrcode
                 ,'acct_id'=>$_POST['acct_id']
