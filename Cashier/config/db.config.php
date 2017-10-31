a68275c7ebfb86df286e596b4a8929da44cd53b8
aa <907810344@qq.cp>
2017-10-31 15:14:18 +0800

    1

--- Cashier/config/db.config.php
+++ Cashier/config/db.config.php
@@ -1,16 +1,21 @@
 <?php
 return array (
 	'default' => array (
-		'hostname' => '127.0.0.1',
-		'database' => 'pay',
-		'username' => 'root',
-		'password' => 'root',
+// 		'hostname' => 'gz-cdb-gaibio6p.sql.tencentcdb.com',
+// 		'database' => 'pay',
+// 		'username' => 'pay',
+// 		'password' => 'ds_5664_ddOOOO',
+        'hostname'=>'rm-2ze9v7b382ggvr23ro.mysql.rds.aliyuncs.com',
+	    'database' => 'pay',
+	    'username' => 'jzzfmydb',
+	    'password' => 'JIhua43426006',
 		'tablepre' => 'cqcjcm_',
 		'charset' => 'utf8',
 		'type' => 'mysql',
 		'comefrom' => 'cqcjcm',
 		'debug' => true,
 		'pconnect' => 0,
+ 	    //'port'=>'3306',
 		'autoconnect' => 0
 	)
 );
